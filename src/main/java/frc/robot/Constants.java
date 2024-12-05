59838162d73c2879e6ddf6873e2b1739742cd426
camden-git <64759244+camden-git@users.noreply.github.com>
2024-12-05 16:17:59 -0600

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,26 @@
+package frc.robot;
+
+import com.pathplanner.lib.util.PIDConstants;
+
+public class Constants {
+    public static class DrivetrainConstants {
+        /* Specifies the Driver Station port index for the driver's controller. */
+        public static final int driveControllerPort = 0;
+        /* Joystick deadband that will be applied to the drivetrain inputs. */
+        public static final double stickDeadband = 0.1;
+
+        /* Maximum drivetrain speed in meters per second. */
+        public static final double maxSpeed = 4.5;
+        /* Number of seconds to wait after the robot is disabled before setting drive motors to neutral. */
+        /* Allows the robot to stop instantly, but is easier to push after the delay. */
+        public static final int disableBreakTime = 10;
+
+        /* PID Constants passed to PathPlanner. */
+        public static final PIDConstants autonTranslationPID = new PIDConstants(1, 0, 0);
+        public static final PIDConstants autonAnglePID = new PIDConstants(1, 0, 0);
+
+    }
+    public class Vision{
+            public static final String frontLimelightName = "limelight-front";
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import java.io.File;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.lib.util.LimelightHelpers;
+import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.Vision;
+import frc.robot.commands.VisionCommands;
+import frc.robot.subsystems.SwerveSubsystem;
+
+
+public class RobotContainer {
+
+  public static final CommandXboxController driveController = new CommandXboxController(Constants.DrivetrainConstants.driveControllerPort);
+  
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem(
+    new File(Filesystem.getDeployDirectory(), "swerve")
+  );
+
+  private final SendableChooser<Command> autoChooser;
+
+  public RobotContainer() {
+    autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("PathPlanner Auto", autoChooser);
+
+    configureBindings();
+  }
+
+  private void configureBindings() {
+    driveController.a().onTrue(Commands.runOnce(swerveSubsystem::zeroGyro));
+
+    driveController.b().onTrue(VisionCommands.aimAtTarget(Constants.Vision.frontLimelightName, swerveSubsystem));
+  }
+
+  public Command getAutonomousCommand() {
+    //return autoChooser.getSelected();
+    return VisionCommands.aimAtTarget("limelight-front", swerveSubsystem); 
+  }
+
+  public Command getTeleopCommand() {
+    /* FieldOrientedAngularVelocity will set the robot's angular velocity based on the right stick. */
+    /* Is field oriented unless right bumper is pressed. */
+
+    return swerveSubsystem.driveCommandAngularVelocity(
+      () -> MathUtil.applyDeadband(-driveController.getLeftY()*0.7, DrivetrainConstants.stickDeadband),
+      () -> MathUtil.applyDeadband(-driveController.getLeftX()*0.7, DrivetrainConstants.stickDeadband),
+      () -> MathUtil.applyDeadband(-driveController.getRightX(), DrivetrainConstants.stickDeadband),
+      () -> driveController.rightBumper().getAsBoolean()
+    ).alongWith(Commands.run(() -> swerveSubsystem.updateOdometryWithVision("limelight-supadog")));
+  }
+
+  
+  public void setMotorBrake(boolean brake)
+  {
+    swerveSubsystem.setMotorBrake(brake);
+  }
+
+  public void stop()
+  {
+    swerveSubsystem.stop();
+  }
+
+}
