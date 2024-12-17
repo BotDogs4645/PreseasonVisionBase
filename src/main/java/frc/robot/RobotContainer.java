ddb64ac61fb53488b45e0d2dc5dcf931abf2d4a4
zachb1807 <56662213+zachb1807@users.noreply.github.com>
2024-12-17 16:20:32 -0600

    refactor(vision): organized robot Commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,10 +14,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.lib.util.LimelightHelpers;
 import frc.robot.Constants.DrivetrainConstants;
-import frc.robot.Constants.Vision;
-import frc.robot.commands.VisionCommands;
+import frc.robot.commands.CommandBuilder;
 import frc.robot.subsystems.SwerveSubsystem;
 
 
@@ -41,12 +39,12 @@ public class RobotContainer {
   private void configureBindings() {
     driveController.a().onTrue(Commands.runOnce(swerveSubsystem::zeroGyro));
 
-    driveController.b().toggleOnTrue(VisionCommands.aimAtNote(Constants.Vision.frontLimelightName, swerveSubsystem));
+    driveController.b().toggleOnTrue(CommandBuilder.Vision.aimAtNote(Constants.Vision.frontLimelightName, swerveSubsystem));
   }
 
   public Command getAutonomousCommand() {
     //return autoChooser.getSelected();
-    return VisionCommands.aimAtTarget("limelight-front", swerveSubsystem); 
+    return CommandBuilder.Vision.aimAtTarget("limelight-front", swerveSubsystem); 
   }
 
   public Command getTeleopCommand() {
---
+++ src/main/java/frc/robot/commands/CommandBuilder.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.lib.util.LimelightHelpers;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class CommandBuilder {
+   
+    public class Vision {
+
+        public static Command aimAtTarget(String limelightName, SwerveSubsystem swerveSubsystem) {
+            return Commands.run(() -> {
+                double[] targetPose = LimelightHelpers.getTargetPose_RobotSpace(limelightName);
+                if (targetPose.length > 1 && (targetPose[0] != 0 || targetPose[1] != 0)) {
+                    //Get the tag's offset from the robot
+                    Rotation2d tagRotation = new Rotation2d(targetPose[0], targetPose[1]).plus(Rotation2d.fromDegrees(90));
+                    swerveSubsystem.drive(new Translation2d(Math.hypot(targetPose[0], targetPose[1]) * 0.4,0), tagRotation.getRadians() * -2, false);
+                } else {
+                swerveSubsystem.drive(new Translation2d(0,0), 0, false);
+                }
+            });
+        }
+
+        public static Command aimAtNote(String limelightName, SwerveSubsystem swerveSubsystem) {
+            return Commands.run(() -> {
+                double tx = LimelightHelpers.getTX(limelightName);
+                if(Math.abs(tx) > 5) {
+                    swerveSubsystem.drive(new Translation2d(-0.3,0), Math.toRadians(tx) * -2, false);
+
+                }
+                else if(Math.abs(tx) > 0) {
+                    swerveSubsystem.drive(new Translation2d(-0.3,0), 0, false);
+                }
+                else {
+                    swerveSubsystem.drive(new Translation2d(0,0), 0, false);
+                }
+            });
+        } 
+    }
+
+}
